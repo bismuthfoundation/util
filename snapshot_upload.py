0b07cb5f7ec34ed43c2c0cf6205d35dae35fc566
geho2 <heat.gh@hovland.name>
2018-12-18 20:05:59 +0100

    Added check for valid ledger

--- snapshot_upload.py
+++ snapshot_upload.py
@@ -1,14 +1,22 @@
-# Script for uploading a ledger snapshot using AWS
-# Script should be run in the main ~/Bismuth folder
-# Requires two files: snapshot.json including variable DB_PATH
-#                     ledger.json created by snapshot_create.py
-#
-# To run as a screen job, requires PATH to be defined, for example:
-#
-# #!/bin/bash
-# PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
-# cd /root/Bismuth
-# screen -d -mS aws_upload python3 snapshot_upload.py
+"""
+Script for uploading a ledger snapshot using AWS
+Script should be run in the main ~/Bismuth folder
+Requires two files: snapshot.json including variable DB_PATH
+                    ledger.json created by snapshot_create.py
+
+Complete script mysnap for snapshot process (between -----):
+
+-----
+#!/bin/bash
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+cd /root/Bismuth
+python3 snapshot_create.py
+python3 ledger_verify.py
+python3 snapshot_upload.py
+-----
+
+The script above can be run as a cron entry: 30 19 * * * screen -d -mS mysnap /root/Bismuth/mysnap
+"""
 
 import json
 import subprocess
@@ -19,6 +27,13 @@ with open('snapshot.json') as json_data:
 with open("{}/ledger.json".format(config['DB_PATH'])) as json_data:
     data = json.load(json_data)
 
-cmd="aws s3 cp {}{} {} --acl public-read".format(config['DB_PATH'],data['filename'],config['bucket'])
-print(cmd)
-push=subprocess.Popen(cmd, shell=True)
+if data['valid'] == 'valid':
+    cmd="aws s3 cp {}{} {} --acl public-read".format(config['DB_PATH'],data['filename'],config['bucket'])
+    print(cmd)
+    push=subprocess.Popen(cmd, shell=True)
+    time.sleep(30)
+
+    url="https://hypernodes.bismuth.live/snapshot_reg.php?url={}&timestamp={}&sha256={}&block_height={}".format(data['url'],data['timestamp'],data['sha256'],data['block_height'])
+    r = requests.get(url)
+else:
+    print('Ledger invalid')
