d2ce53c2ce1eaa29c8827b8ca0be578d8363b9a9
geho2 <26554545+geho2@users.noreply.github.com>
2019-06-27 17:11:33 +0200

    Create hn_config.py
    Configuration variables used by hn_monitor.py

---
+++ hypernode_monitoring/hn_config.py
@@ -0,0 +1,14 @@
+"""
+Bismuth
+Configuration variables for HN monitoring script
+"""
+
+IP="127.0.0.1"
+PORT="6969"
+HN_PATH="/root/hypernode/modules"
+HN_ADDRESS="your_hypernode_address_here_starts_with_a_B"
+TIMEOUT_1=20
+TIMEOUT_2=30
+OUTFILE_1="/path/status.json"
+OUTFILE_2="/path/hypernodes.json"
+OUTFILE_3="/path/status_ex.json"
