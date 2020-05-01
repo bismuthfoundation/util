b0a0fb7bf8b89c1cdfef675daf84cf27d56947cc
geho2 <26554545+geho2@users.noreply.github.com>
2020-05-01 07:02:10 +0200

    Create clone_all.sh
    Archives all the repos of the Bismuthfoundation

---
+++ clone_all.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# This script clones all the repos of the Bismuthfoundation
+# usage:
+#     sudo apt-get install curl
+#     mkdir archive
+#     cd archive
+#     wget https://raw.githubusercontent.com/bismuthfoundation/util/master/clone_all.sh
+#     chmod u+x clone_all.sh
+#     ./clone_all.sh
+
+CNTX={orgs}; NAME={Bismuthfoundation}; PAGE=1
+curl "https://api.github.com/$CNTX/$NAME/repos?page=$PAGE&per_page=100" |
+  grep -e 'git_url*' |
+  cut -d \" -f 4 |
+  xargs -L1 git clone
