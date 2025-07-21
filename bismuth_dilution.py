41632c3b149c78c9127d7acf999f49401cfed6ba
alias-bitsignal <39199608+alias-bitsignal@users.noreply.github.com>
2025-07-21 16:29:12 +0200

    Remove pos_reward & dev_reward from dilution calculations
    Since block height 4380000 pos_reward + dev_reward are removed.
    
    Source:
    https://medium.com/@bismuthplatform/upcoming-bismuth-soft-fork-at-block-4-380-000-b50e4259ccfe

--- bismuth_dilution.py
+++ bismuth_dilution.py
@@ -2,6 +2,7 @@
 Script for calculating yearly dilution
 of the Bismuth cryptocurrency
 See also: https://hypernodes.bismuth.live/?p=218 
+https://medium.com/@bismuthplatform/upcoming-bismuth-soft-fork-at-block-4-380-000-b50e4259ccfe
 """
 
 def calc_rewards():
@@ -9,27 +10,39 @@ def calc_rewards():
     HF1 = 800000  # Block height at hard fork 1
     HF2 = 1200000 # Block height at hard fork 2
     HF3 = 1450000 # Block height at hard fork 3
-    for block in range(1440*365*100): # 100 years, 1440 blocks per day
+    HF4 = 4380000 # block height at soft fork
+
+    for block in range(1440*365*100):
+        # determine pow_reward & pos_reward
         if block < HF1:
             pow_reward = 15.0 - block/1e6
-            pos_reward = 0
+            pos_reward = 0.0
+
         elif block < HF2:
             pow_reward = 15.0 - block/(1e6/2) - 0.8
             pos_reward = 0.8
-        elif block < HF3:
-            pow_reward = (11.8-1.6)-(block-HF2)/(0.5e6)
-            pos_reward = 2.4
-        else:
-            pow_reward = 5.5 -(block-HF3)/(1.1e6)
-            pos_reward = 2.4 -(block-HF3)/(3.0e6)
-            # Proposed long-term minimum rewards
-            if pow_reward < 0.5:
-                pow_reward = 0.5
-            if pos_reward < 0.5:
-                pos_reward = 0.5
 
+        elif block < HF3:
+            pow_reward = (11.8-1.6) - (block-HF2)/(0.5e6)
+            pos_reward = 2.4
+
+        else:
+            pow_reward = 5.5 - (block-HF3)/(1.1e6)
+            pos_reward  = 2.4 - (block-HF3)/(3.0e6)
+            # enforce long-term minimum rewards
+            pow_reward = max(pow_reward, 0.5)
+            pos_reward = max(pos_reward, 0.5)
+
+        # base dev cut for *all* blocks < HF4
         dev_reward = 0.1 * pow_reward
-        rewards.append(pow_reward + dev_reward + pos_reward)
+
+        # override at HF4: drop PoS & dev entirely
+        if block >= HF4:
+            pos_reward = 0.0
+            dev_reward = 0.0
+
+        rewards.append(pow_reward + pos_reward + dev_reward)
+
     return rewards
 
 if __name__ == '__main__':
